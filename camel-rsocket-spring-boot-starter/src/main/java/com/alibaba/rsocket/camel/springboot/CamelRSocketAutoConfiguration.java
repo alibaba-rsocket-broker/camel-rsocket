a28317269476a19458d549174447e258a1d6dc5a
linux_china <libing.chen@gmail.com>
2021-08-28 17:55:47 -0700

    Camel RSocket Component Auto Configuration

---
+++ camel-rsocket-spring-boot-starter/src/main/java/com/alibaba/rsocket/camel/springboot/CamelRSocketAutoConfiguration.java
@@ -0,0 +1,13 @@
+package com.alibaba.rsocket.camel.springboot;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Camel RSocket Component Auto Configuration
+ *
+ * @author leijuan
+ */
+@Configuration
+public class CamelRSocketAutoConfiguration {
+
+}
